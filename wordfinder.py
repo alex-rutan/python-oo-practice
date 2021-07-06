653083c6d9e8c3248a4d9dbb9687d55a1742a320
magus0 <harvestashya@gmail.com>
2021-07-06 16:00:49 -0700

    start the word finder

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,24 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, file_path):
+        """Initialize taking in a file path
+        creating a words attribute which is a list of words
+        from the file read. Then it prints how many words
+        were read from the file."""
+        self.words = []
+        with open(file_path, "r") as file:
+            for line in file:
+                self.words.append(line[:-1])
+        
+        self.printWordsRead()
+        
+    def printWordsRead(self):
+        """Prints a count of how many words are in this instance of WordFider"""
+        print(f"{len(self.words)} words read")
+        
+    def random(self):
+        """returns a single random word from the words attribute."""
+        return choice(self.words)
+        
