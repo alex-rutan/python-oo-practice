865c47705fd6d94fe612eaa49d620f8cd6b4d6fa
Alex  Rutan <alexrutan04@gmail.com>
2021-07-06 15:24:51 -0700

    1st problem

--- serial.py
+++ serial.py
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        '''takes in start integer, sets current counter value'''
+        self.start = start
+        self.last_number = start - 1
+
+    def generate(self):
+        '''increases current count value, returns the current count'''
+        self.last_number += 1
+        return self.last_number 
+
+    def reset(self):
+        '''resets the current count value back to the original start, start - 1'''
+        self.last_number = self.start - 1
+        
+    
+
