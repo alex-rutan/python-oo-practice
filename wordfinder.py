d3802d4d1cd58fa5d8860c156abae4c5ea3714c0
Alex  Rutan <alexrutan04@gmail.com>
2021-07-06 17:00:35 -0700

    nearly done problem 3

--- wordfinder.py
+++ wordfinder.py
@@ -7,18 +7,28 @@ class WordFinder:
         creating a words attribute which is a list of words
         from the file read. Then it prints how many words
         were read from the file."""
-        self.words = []
-        with open(file_path, "r") as file:
-            for line in file:
-                self.words.append(line[:-1])
-        
+        self.words = self.get_words(file_path)
+
         self.printWordsRead()
-        
+
+    def get_words(self, file_path):
+        with open(file_path, "r") as file:
+            return [line.strip() for line in file.readlines()]
+    
     def printWordsRead(self):
-        """Prints a count of how many words are in this instance of WordFider"""
+        """Prints a count of how many words are in this instance of WordFinder"""
         print(f"{len(self.words)} words read")
         
     def random(self):
         """returns a single random word from the words attribute."""
         return choice(self.words)
         
+
+
+class SpecialWordFinder(WordFinder):
+    '''finds random words from file with listed words, 
+    while skipping blank lines and lines that start with "#" '''
+    
+    def get_words(self, file_path):
+        with open(file_path, "r") as file:
+            return [line[:-1] for line in file if line.strip() and line[0] != "#"] 
