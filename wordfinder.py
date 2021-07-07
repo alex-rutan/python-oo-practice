3ea0c0a2a10ebd4369b080a0a4c947fd1a9141ab
magus0 <harvestashya@gmail.com>
2021-07-06 17:12:10 -0700

    I'm fancy

--- wordfinder.py
+++ wordfinder.py
@@ -3,21 +3,25 @@ from random import choice
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self, file_path):
-        """Initialize taking in a file path
-        creating a words attribute which is a list of words
-        from the file read. Then it prints how many words
-        were read from the file."""
+        """Initialize WordFinder taking in a file path
+        Makes a words attribute from words in the file
+        Then it prints how many words were read from the file."""
         self.words = self.get_words(file_path)
-
-        self.printWordsRead()
+        print(self.print_Words_Read())
+        
+    def __repr__(self):
+        """instead of showing position in memory, shows how many words read"""
+        return self.print_Words_Read()
 
     def get_words(self, file_path):
+        """takes in a file path, returns a list of words
+        from the file read."""
         with open(file_path, "r") as file:
             return [line.strip() for line in file.readlines()]
     
-    def printWordsRead(self):
+    def print_Words_Read(self):
         """Prints a count of how many words are in this instance of WordFinder"""
-        print(f"{len(self.words)} words read")
+        return f"{len(self.words)} words read"
         
     def random(self):
         """returns a single random word from the words attribute."""
@@ -30,5 +34,9 @@ class SpecialWordFinder(WordFinder):
     while skipping blank lines and lines that start with "#" '''
     
     def get_words(self, file_path):
+        """takes in a file path, returns a list of words
+        from the file read ignoring blank lines and line starting with #"""
         with open(file_path, "r") as file:
-            return [line[:-1] for line in file if line.strip() and line[0] != "#"] 
+            return [line.strip() 
+                for line in file 
+                if line.strip() and line[0] != "#"] 
